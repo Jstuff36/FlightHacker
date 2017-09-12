29373c924d9a6e7d428c1f64e8f9f4ef2b80e967
Jstuff36 <mizzou_fan10@hotmail.com>
2017-09-12 16:02:52 -0700

    Finds best prices

--- flight_hacker.py
+++ flight_hacker.py
@@ -1,12 +1,16 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Bot:
 
     def __init__(self):
         self.browser = webdriver.Firefox(executable_path='./geckodriver')
         # self.browser = webdriver.Chrome('./chromedriver')
+        self.prices = []
         self.run()
 
     def run(self):
@@ -22,7 +26,7 @@ class Bot:
         departure_city = "COU"
         destination_city = "HND"
         departure_take_off_boxes = self.browser.execute_script(
-            "return document.getElementsByClassName('EIGTDNC-Kb-f EIGTDNC-Kb-b')")
+            "return document.querySelectorAll('.EIGTDNC-Kb-f.EIGTDNC-Kb-b')")
         print(departure_take_off_boxes[0].get_attribute('outerHTML'))
         print(departure_take_off_boxes[1].get_attribute('outerHTML'))
         departure_take_off_boxes[0].send_keys(departure_city)
@@ -32,7 +36,11 @@ class Bot:
         departure_take_off_boxes[1].send_keys(Keys.RETURN)
         time.sleep(1)
 
-
+    def findPrices(self):
+        self.browser.implicitly_wait(10) 
+        self.prices = self.browser.execute_script(
+            "return document.querySelectorAll('.EIGTDNC-d-bc')")
+        print(self.prices)
 
 
 
